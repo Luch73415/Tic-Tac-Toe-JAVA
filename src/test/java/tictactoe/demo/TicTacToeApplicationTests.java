932b7fa1b9bf38fbd55a588374af852468ac75c8
Lucas Pinho Furlan <lucaspfurlan@uol.com.br>
2021-03-05 11:46:36 -0300

    First Commit Java Game Tic Tac Toe

---
+++ src/test/java/tictactoe/demo/TicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package tictactoe.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
